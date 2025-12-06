7c842526d0d323aa649217b84773f75aafe092de
semens901 <semen.aleksandrov.semen@gmail.com>
2025-12-06 04:27:25 +0300

    added 3 implementations of probability samples

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/simon/Numera
-# Build directory: /home/simon/Numera/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("numera")
-subdirs("examples")
-subdirs("tests")
